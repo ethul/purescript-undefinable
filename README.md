2f4f1c9e55e35f233f587772ff4ec393ecbeac53
eric thul <thul.eric@gmail.com>
2016-05-01 12:36:48 -0400

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # purescript-undefinable
+
+A library for handling `undefined` values.
+
+## Installation
+
+```bash
+bower install --save purescript-undefinable
+```
---
+++ src/Data/Undefinable.js
@@ -0,0 +1,14 @@
+/* global exports */
+'use strict';
+
+// module Data.Undefinable
+
+exports['undefined'] = undefined;
+
+exports.undefinable = function(nothing, just, value){
+  return value === undefined ? nothing : just(value);
+};
+
+exports.notUndefined = function(value){
+  return value;
+};
